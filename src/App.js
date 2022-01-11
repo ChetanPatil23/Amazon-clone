139b847c8640ed6ba8405e66b56d346576477c48
ChetanPatil23 <chetan08rp@gmail.com>
2022-01-11 19:04:23 +0530

    Navbar_Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route path="/checkout">Checkout</Route>
-          <Route path="/login">Login</Route>
-          <Route path="/">Home</Route>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Home Component</h1>
+          </Route>
         </Switch>
       </div>
     </Router>
