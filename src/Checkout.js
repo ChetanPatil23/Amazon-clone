885f6eccc6c1c172527c618450ad86f7ac2bc36b
ChetanPatil23 <chetan08rp@gmail.com>
2022-01-14 23:12:59 +0530

    Subtotal component

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,32 +1,40 @@
 import React from "react";
 import "./Checkout.css";
 import { useGlobalContext } from "./Context";
-import CheckoutProduct from './CheckoutProduct'
+import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from './Subtotal';
 
 const Checkout = () => {
   const [{ basket }, dispatch] = useGlobalContext();
 
   return (
     <div className="checkout">
-      <img
-        className="checkout_ad"
-        src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-        alt="Checkout_Advertisement"
-      />
-      {basket?.length === 0 ? (
-        <div>
-          <h1>Your Shopping Basket is empty</h1>
-          <p>
-            You have no items in your basket. Please add items by clicking 'Add
-            to Basket' button from home page
-          </p>
-        </div>
-      ) : (
-        <div>
-          <h1 className="checkout_title">Your Shopping Basket</h1>
-          {basket.map((item) => (
-            <CheckoutProduct key={item.id} {...item} />
-          ))}
+      <div className="checkoutLeft">
+        <img
+          className="checkout_ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt="Checkout_Advertisement"
+        />
+        {basket?.length === 0 ? (
+          <div>
+            <h1>Your Shopping Basket is empty</h1>
+            <p>
+              You have no items in your basket. Please add items by clicking
+              'Add to Basket' button from home page
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h1 className="checkout_title">Your Shopping Basket</h1>
+            {basket.map((item) => (
+              <CheckoutProduct key={item.id} {...item} />
+            ))}
+          </div>
+        )}
+      </div>
+      {basket.length > 0 && (
+        <div className="checkoutRight">
+          <Subtotal/>
         </div>
       )}
     </div>
---
+++ src/Subtotal.js
@@ -0,0 +1,41 @@
+import React, {useState,useEffect} from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useGlobalContext } from "./Context";
+import { getBasketTotal } from "./reducer";
+
+const Subtotal = () => {
+  const [{ basket }, dispatch] = useGlobalContext();
+// //   console.log(getBasketTotal(basket));
+// const [price,setPrice]=useState(0);
+// useEffect(() => {
+//     const newPrice=basket?.reduce((amount,item)=>{return (Number(item.price)+amount)},0);
+//     setPrice(newPrice)
+// }, [basket])
+// console.log(price);
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items) <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+};
+
+export default Subtotal;
